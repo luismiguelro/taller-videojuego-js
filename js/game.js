53bf602758259281af22212d3c85ab7f03762618
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-05-10 10:27:39 -0500

    Canvas responsive

--- js/game.js
+++ js/game.js
@@ -1,34 +1,39 @@
 const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d'); // contexto: 2 dimensiones (x,y)
 
-// contexto: 2 dimensiones (x,y)
-const game = canvas.getContext('2d');
 
-// una vez cargue el HTMl
-window.addEventListener('load',starGame);
-// inicializar juego
-function starGame(){
-    // asignar el size de la pantalla
-    let canvasSize;
+let canvasSize;
+let elementsSize;
 
+window.addEventListener('load',setCanvasSize);// una vez cargue el HTMl
+
+window.addEventListener('resize',setCanvasSize);// resize del canvas
+
+function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.7;
-    } else {
-        canvasSize = window.innerHeight * 0.7;
-    }
-    
+        canvasSize = window.innerWidth * 0.8;
+      } else {
+        canvasSize = window.innerHeight * 0.8;
+      }
+      
+      canvas.setAttribute('width', canvasSize);
+      canvas.setAttribute('height', canvasSize);
+      
+      elementsSize = canvasSize / 10;
+  
+    startGame();
+  }
+// inicializar juego
+
+function startGame(){
    
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('height', canvasSize);
-
-    // calcular tamaño elemento 
-    const elementsSize = (canvasSize / 10)-1;
-    console.log({ canvasSize, elementsSize });
-
     game.font = elementsSize + 'px Verdana';
-    game.textAlign = 'end';
+    game.textAlign = "";
 
-    for (let i = 1; i <= 10; i++) {
-        game.fillText(emojis['X'], elementsSize, elementsSize * i);
+    for (let i = 0; i <= 10; i++) {
+        for (let j = 1; j < 11; j++) {
+            game.fillText(emojis['X'], elementsSize*i, elementsSize*j);
+        }
+       
       }
 }
-
