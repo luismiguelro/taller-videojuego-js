017be55d0438ac92091759402d5e997f0ec5ce2a
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-05-17 09:51:58 -0500

    Renderizar mapa

--- js/game.js
+++ js/game.js
@@ -10,30 +10,47 @@ window.addEventListener('load',setCanvasSize);// una vez cargue el HTMl
 window.addEventListener('resize',setCanvasSize);// resize del canvas
 
 function setCanvasSize() {
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
-      } else {
-        canvasSize = window.innerHeight * 0.8;
-      }
-      
-      canvas.setAttribute('width', canvasSize);
-      canvas.setAttribute('height', canvasSize);
-      
-      elementsSize = canvasSize / 10;
-  
-    startGame();
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
   }
-// inicializar juego
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
 
+   elementsSize = canvasSize / 10;
+
+  startGame();
+}
+
+// inicializar juego
 function startGame(){
    
-    game.font = elementsSize + 'px Verdana';
-    game.textAlign = "";
+    game.font = (elementsSize-6) + 'px Verdana';
+    game.textAlign = "end";
 
-    for (let i = 0; i <= 10; i++) {
-        for (let j = 1; j < 11; j++) {
-            game.fillText(emojis['X'], elementsSize*i, elementsSize*j);
-        }
-       
-      }
+    // Recorrer arreglo
+  const map = maps[2];
+
+  /*
+  Crear un arreglo cada vez que se tenga un salto de linea
+    - .split: genera un arreglo por cada elemento)
+    - .trim (metodo de los strigns): ayuda a limpiar los espacios en blanco */
+
+    // obtener filas del mapa
+  const mapRows = map.trim().split('\n');
+
+  /*obtener columnas : se obtiene un array de array
+      - row: representa cada elemento de la fila, y a cada string (row)
+      se ejecuta el .trim
+  */
+
+  // crear nuevo arreglo
+  const mapRowCols = mapRows.map(row=>row.trim().split(''));
+    
+    for (let row = 1; row <= 10;row++) {
+        for (let col= 1;col <= 10; col++) {
+          game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+      }   
+    }
 }
